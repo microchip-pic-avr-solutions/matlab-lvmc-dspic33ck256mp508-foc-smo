de739462f1023057c14243292890b03900b465c4
I52209 <Hithesh.CN@microchip.com>
2022-06-03 10:31:53 +0530

    Adding SMO based models from Prosenjit converted from encoder model. This model has been tested on Hurst300, Hurst075 and Linix motors. The motor selection has to be done on .m file

--- matlab-lvmc-dspic33ck256mp508-foc-smo/dsPIC33CK256MP508_SMO_LVMC_data.m
+++ matlab-lvmc-dspic33ck256mp508-foc-smo/dsPIC33CK256MP508_SMO_LVMC_data.m
@@ -1,7 +1,7 @@
 %% ************************************************************************
 % Model         :   PMSM Field Oriented Control
 % Description   :   Set Parameters for PMSM Field Oriented Control
-% File name     :   mcb_pmsm_foc_qep_f28069LaunchPad_data.m
+% File name     :   mcb_pmsm_foc_sensorless_dsPIC33_data.m
 % Copyright 2020 The MathWorks, Inc.
 
 %% Simulation Parameters
@@ -16,7 +16,6 @@ Ts_simulink     = T_pwm/2;      %sec        // simulation time step for model si
 Ts_motor        = T_pwm/2;      %Sec        // Simulation sample time
 Ts_inverter     = T_pwm/2;      %sec        // simulation time step for average value inverter
 Ts_speed        = 30*Ts;        %Sec        // Sample time for speed controller
-% Ts_speed        = 10e-3;      %Sec        // Sample time for speed controller
 
 %% Set data type for controller & code-gen
 dataType = fixdt(1,16,14);    % Fixed point code-generation
@@ -26,36 +25,74 @@ dataType2 = fixdt(1,16,12);    % Fixed point code-generation
 
 %% System Parameters
 % Set motor parameters
-pmsm.model  = 'Hurst';          %           // Manufacturer Model Number
+
+%Long Hurst Motor (Uncomment while using this motor from line below)
+pmsm.model  = 'Hurst 300';      %           // Manufacturer Model Number
 pmsm.sn     = '123456';         %           // Manufacturer Model Number
 pmsm.p = 5;                     %           // Pole Pairs for the motor
-pmsm.Rs = 0.4758;               %Ohm        // Stator Resistor
-pmsm.Ld = 2.8698e-4;            %H          // D-axis inductance value
-pmsm.Lq = 2.8470e-4;            %H          // Q-axis inductance value
+pmsm.Rs = 0.285;                %Ohm        // Stator Resistor
+pmsm.Ld = 2.8698e-4;              %H          // D-axis inductance value
+pmsm.Lq = 2.8698e-4;              %H          // Q-axis inductance value
 pmsm.Ke = 7.3425;               %Bemf Const	// Vline_peak/krpm
-pmsm.J = 2.0148e-5;             %Kg-m2      // Inertia in SI units
-pmsm.B = 7.0130e-5;             %Kg-m2/s    // Friction Co-efficient
-pmsm.I_rated= 3.4;              %A      	// Rated current (phase-peak)
+pmsm.Kt = 0.274;                %Nm/A       // Torque constant
+pmsm.J = 7.061551833333e-6;     %Kg-m2      // Inertia in SI units
+pmsm.B = 2.636875217824e-6;     %Kg-m2/s    // Friction Co-efficient
+pmsm.I_rated= 3.42*sqrt(2);     %A      	// Rated current (phase-peak)
 pmsm.QEPSlits = 1000;           %           // QEP Encoder Slits
-pmsm.N_base = 2800;             %rpm        // Base speed
 pmsm.N_max  = 2000;             %rpm        // Max speed
 pmsm.FluxPM     = (pmsm.Ke)/(sqrt(3)*2*pi*1000*pmsm.p/60); %PM flux computed from Ke
 pmsm.T_rated    = (3/2)*pmsm.p*pmsm.FluxPM*pmsm.I_rated;   %Get T_rated from I_rated
 
+%Short Hurst Motor (Uncomment while using this motor from line below)
+% pmsm.model  = 'Short Hurst';    %           // Manufacturer Model Number
+% pmsm.sn     = '123456';         %           // Manufacturer Model Number
+% pmsm.p = 5;                     %           // Pole Pairs for the motor
+% pmsm.Rs = 2.8;                  %Ohm        // Stator Resistor
+% pmsm.Ld = 0.002423;             %H          // D-axis inductance value
+% pmsm.Lq = pmsm.Ld;              %H          // Q-axis inductance value
+% pmsm.Ke = 7.24;                 %Bemf Const	// Vline_peak/krpm
+% pmsm.Kt = 0.03;                %Nm/A       // Torque constant
+% pmsm.J = 5.5e-6;     %Kg-m2      // Inertia in SI units
+% pmsm.B = 2.636875217824e-6/2;     %Kg-m2/s    // Friction Co-efficient
+% pmsm.I_rated= 1.2*sqrt(2);     %A      	// Rated current (phase-peak)
+% pmsm.QEPSlits = 1000;           %           // QEP Encoder Slits
+% pmsm.N_max  = 2000;             %rpm        // Max speed
+% pmsm.FluxPM     = (pmsm.Ke)/(sqrt(3)*2*pi*1000*pmsm.p/60); %PM flux computed from Ke
+% pmsm.T_rated    = (3/2)*pmsm.p*pmsm.FluxPM*pmsm.I_rated;   %Get T_rated from I_rated
+
+%Linix Motor (Uncomment while using this motor from line below)
+% pmsm.model  = 'Linix';          %           // Manufacturer Model Number
+% pmsm.sn     = '45ZWN24-40';     %           // Manufacturer Model Number
+% pmsm.p = 2;                     %           // Pole Pairs for the motor
+% pmsm.Rs = 0.569240;             %Ohm        // Stator Resistor
+% pmsm.Ld = 0.00039;              %H          // D-axis inductance value
+% pmsm.Lq = 0.00039;              %H          // Q-axis inductance value
+% pmsm.Ke = 2.97;               %Bemf Const	// Vline_peak/krpm
+% pmsm.Kt = 0.03;                %Nm/A       // Torque constant
+% pmsm.J = 3.86e-6;     %Kg-m2      // Inertia in SI units
+% pmsm.B = 11.09e-6;     %Kg-m2/s    // Friction Co-efficient
+% pmsm.I_rated= 2.2*sqrt(2);     %A      	// Rated current (phase-peak)
+% pmsm.QEPSlits = 1000;           %           // QEP Encoder Slits
+% pmsm.N_max  = 4000;             %rpm        // Max speed
+% pmsm.FluxPM     = (pmsm.Ke)/(sqrt(3)*2*pi*1000*pmsm.p/60); %PM flux computed from Ke
+% pmsm.T_rated    = (3/2)*pmsm.p*pmsm.FluxPM*pmsm.I_rated;   %Get T_rated from I_rated
+
+
 %% Inverter parameters
 
-inverter.model         = 'dsPIC33CK LVMC';               % 		// Manufacturer Model Number
+inverter.model         = 'LVMC';         % 		// Manufacturer Model Number
 inverter.sn            = 'INV_XXXX';         		% 		// Manufacturer Serial Number
 inverter.V_dc          = 24;       					%V      // DC Link Voltage of the Inverter
-inverter.I_max         = 4.4; 						%Amps   // Max current that can be measured
-inverter.I_trip        = 3.5;                 		%Amps   // Max current for trip
+inverter.ISenseMax     = 21.85; 					%Amps   // Max current that can be measured
+inverter.I_trip        = 10;                  		%Amps   // Max current for trip
 inverter.Rds_on        = 1e-3;                      %Ohms   // Rds ON
-inverter.Rshunt        = 0.025;                     %Ohms   // Rshunt
+inverter.Rshunt        = 0.01;                     %Ohms   // Rshunt
 inverter.R_board        = inverter.Rds_on + inverter.Rshunt/3;  %Ohms
 %Note: inverter.I_max = 1.65V/(Rshunt*10V/V) for 3.3V ADC with offset of 1.65V
 %This is modified to match 3V ADC with 1.65V offset value for %LaunchXL-F28379D
 inverter.MaxADCCnt     = 4095;      				%Counts // ADC Counts Max Value
 inverter.invertingAmp  = -1;                        % 		//Non inverting current measurement amplifier
+inverter.deadtime      = 1e-9;
 
 %% Derive Characteristics
 pmsm.N_base = mcb_getBaseSpeed(pmsm,inverter); %rpm // Base speed of motor at given Vdc
@@ -66,8 +103,8 @@ pmsm.N_base = mcb_getBaseSpeed(pmsm,inverter); %rpm // Base speed of motor at gi
 PU_System = mcb_SetPUSystem(pmsm,inverter);
 
 %% Controller design // Get ballpark values!
-
-PI_params = mcb.internal.SetControllerParameters(pmsm,inverter,PU_System,T_pwm,Ts,3*Ts_speed);
+% Get PI Gains
+PI_params = mcb.internal.SetControllerParameters(pmsm,inverter,PU_System,T_pwm,Ts,Ts_speed);
 
 %Updating delays for simulation
 PI_params.delay_Currents    = int32(Ts/Ts_simulink);
@@ -76,3 +113,8 @@ PI_params.delay_Speed       = int32(Ts_speed/Ts_simulink);
 PI_params.delay_Speed1       = (PI_params.delay_IIR + 0.5*Ts)/Ts_speed;
 
 % mcb_getControlAnalysis(pmsm,inverter,PU_System,PI_params,Ts,Ts_speed);
+
+%% Open loop reference values
+T_Ref_openLoop          = 1;                    % Sec // Time for open-loop start-up
+Speed_openLoop          = 500;                  % PU  // Per-Unit speed referene for open-loop start-up
+%Vd_Ref_openLoop_PU      = Speed_openLoop_PU;    % Use 1.2x for Dyno setup and 1x for others
